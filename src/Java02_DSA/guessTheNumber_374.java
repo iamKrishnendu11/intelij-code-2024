9b2fd6f33fcda78dd7ea0e3492eafd959595bb27
iamKrishnendu11 <mandalkrishnendu1119@gmail.com>
2026-01-13 21:01:08 +0530

    day 4 ended:- LeetCode rank:- 4,353,054

---
+++ src/Java02_DSA/guessTheNumber_374.java
@@ -0,0 +1,16 @@
+package Java02_DSA;
+/**
+        * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+        * int guess(int num);
+ */
+
+public class guessTheNumber_374 {
+  // check in leetcode
+
+   //  https://leetcode.com/problems/guess-number-higher-or-lower/
+
+}
